0414f097d07ae818df0d4919b0a2a027d4027fe4
Tatiana <tatianaw83@gmail.com>
2020-07-31 11:38:17 -0700

    updated About Me

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -5,7 +5,10 @@ export default class AboutMe extends Component {
     render() {
         return (
             <Grid className="AboutMe-grid" container justify="center" alignContent = 'center' alignItems = 'center'>
-                <Grid item xs={12} md={6} >
+                <Grid item xs={12} md={6} className="AboutMe-sideProfile">
+                <Grid container justify="center" alignContent = 'center' alignItems = 'center' className="sideProfile">
+
+                
                 <Typography variant="h2" component="h1">
                     Tatiana Walton
                 </Typography>
@@ -14,20 +17,27 @@ export default class AboutMe extends Component {
                 </Typography>
                 <img className="AboutMe-img" alt="Tatiana Walton" src = 'https://tatianacodes.s3-us-west-2.amazonaws.com/profile.jpg'/>
                 </Grid>
+                </Grid>
 
                 <Grid item xs={12} md={6}>
                 <Paper className="AboutMe-paper" elevation={4}>
 
                 <Typography className="AboutMe-paragraph" align="center"  paragraph = {true} variant = "body1">
-                    I'm a former high performing adidas Originals team captain turned high organic growth social media manager for a celebrity-headed startup, turned six-figure portfolio Jr. project manager, turned Shopify webstore owner and digital marketer, turned web developer. Needless to say, I have always been a bit of a jack of all trades with a high level of interest in many different fields. Being a multifaceted individual keeps it light and fun for me, and gives me a perspective not many have to offer. I consider myself a lifelong learner and find satisfaction in simply attaining knowledge. 
+                    I've worn many hats in the last 6 years. Adidas Originals team captain while in college, to social media manager for a celebrity-founded health and wellness startup, to an IT Jr. project manager, to an independent Shopify store owner, to MERN stack web developer. Needless to say, I take pride in being a jack of all trades with a lifetime learning mentality. Being a multifaceted individual keeps it light and fun for me, and gives me a perspective not many have to offer.  
                 </Typography>
                 <Divider variant="middle"/>
                 <Typography className="AboutMe-paragraph"  align="center"  paragraph = {true} variant = "body1">
-                    Having experience in multiple areas of tech, I enjoy being able to consult for my clients with a holistic mindset. A strong digital strategy is made up of many parts to create a whole, and all parts must work together!
+                    Due to my diverse professional experience, I enjoy being able to holistically consult with my clients. I address root causes, not symptoms, and believe that a strong foundation relies on interworking parts that perform at a high level. 
                 </Typography>
                 <Divider variant="middle"/>
                 <Typography className="AboutMe-paragraph"  align="center" paragraph = {true} variant = "body1">
-                    In my free time, I collect sneakers and streetwear, as well as enjoy live music, and I love to consult with businesses and folks in those industries. I am based in Orange County, California.
+                    In my free time, I collect sneakers and streetwear, as well as enjoy live music, and so I love to consult with businesses and folks in those industries. I live authentically in my professional life, and bring my whole self to the table. WYSIWYG.
+                </Typography>
+                <Divider variant="middle"/>
+                <Typography className="AboutMe-paragraph"  align="center" paragraph = {true} variant = "body1">
+                   I am based in Orange County, California, but I am happy to travel.
+
+                   Let's chat!
                 </Typography>
                 </Paper>
                 </Grid>
