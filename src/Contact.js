95841e3e0fbb721cc7b8184631be7b027c8682bb
Tatiana <tatianaw83@gmail.com>
2020-08-01 10:28:26 -0700

    updated links

--- src/Contact.js
+++ src/Contact.js
@@ -7,10 +7,14 @@ export default class Contact extends Component {
         return (
                 <Grid className="Contact" container justify="center" alignContent = 'center' alignItems = 'center'>
                     <Grid item xs ={12} md={4}>
+                    <a href="https://www.linkedin.com/in/tatianadwalton/">
                     <img alt ="LinkedIn Logo" src="https://icongr.am/devicon/linkedin-original.svg?size=128&color=currentColor"></img>
+                    </a>
                     </Grid>
                    <Grid item xs = {12} md={4}>
+                    <a href ="https://github.com/twalton83">
                    <img alt ="Github Logo" src="https://icongr.am/devicon/github-original.svg?size=128&color=currentColor"></img>
+                   </a>
                    </Grid>
                    <Grid item xs = {12} md={4}>
                     <a href="mailto:hello@tatianacodes.com">
