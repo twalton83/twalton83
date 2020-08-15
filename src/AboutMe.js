93637c4b1af42f273025fff4c85dc92e63cda3ae
Tatiana <tatianaw83@gmail.com>
2020-08-15 16:37:05 -0700

    udpated aboutme and css for better responsiveness

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -17,25 +17,25 @@ export default class AboutMe extends Component {
                 </Grid>
                 </Grid>
 
-                <Grid item xs={12} md={6}>
+                <Grid className ="paper-container" item xs={12} md={6}>
                 <Paper className="AboutMe-paper" elevation={4}>
 
                 <Typography className="AboutMe-paragraph" align="center"  paragraph = {true} variant = "body1">
-                    I've worn many hats in the last 6 years. Adidas Originals team captain while in college, to social media manager for a celebrity-founded health and wellness startup, to an IT Jr. project manager, to an independent Shopify store owner, to MERN stack web developer. Needless to say, I take pride in being a jack of all trades with a lifetime learning mentality. Being a multifaceted individual keeps it light and fun for me, and gives me a perspective not many have to offer.  
+                    Hey! I'm Tatiana, a MERN stack web developer with a newfound love of Python, and have dabbled briefly in PHP. I am also a certified Shopify Partner and have experience in creating Liquid templates with Theme Kit. I've worn many hats in the last 6 years. I built a foundation in retail streetwear, and soon found myself as the social media manager for a wellness startup founded by a Grammy nominated singer in Hollywood. Shortly after, I became an IT Jr. Project Manager, and somehow came to fall in love with the programming aspect of the field. I take pride in being a jack of all trades with a lifetime learning mentality, and leverage my diverse experience in any role.
                 </Typography>
                 <Divider variant="middle"/>
                 <Typography className="AboutMe-paragraph"  align="center"  paragraph = {true} variant = "body1">
-                    Due to my diverse professional experience, I enjoy being able to holistically consult with my clients. I address root causes, not symptoms, and believe that a strong foundation relies on interworking parts that perform at a high level. 
+                    I enjoy being able to holistically consult with my clients, drawing from those experiences in many fields. I address root causes, not symptoms, and believe that a strong foundation relies on interworking parts that perform at a high level. I uphold a rule for frequent communication, and love to involve even the least technical clients in the process.
                 </Typography>
                 <Divider variant="middle"/>
                 <Typography className="AboutMe-paragraph"  align="center" paragraph = {true} variant = "body1">
-                    In my free time, I collect sneakers and streetwear, as well as enjoy live music, and so I love to consult with businesses and folks in those industries. I live authentically in my professional life, and bring my whole self to the table. WYSIWYG.
+                    In my free time, I collect sneakers and streetwear, as well as enjoy live music, and so I love to consult with clients in those industries. I live authentically in my professional life, and bring my whole self to the table. WYSIWYG, and WYG is a developer with an "ear to the streets" who thrives in a modern, inclusive, and forward-thinking environment. 
                 </Typography>
                 <Divider variant="middle"/>
                 <Typography className="AboutMe-paragraph"  align="center" paragraph = {true} variant = "body1">
-                   I am based in Orange County, California, but I am happy to travel.
+                   I am based in Orange County, California, but I am happy to travel if the project calls for it.
 
-                   Let's chat!
+                   Let's chat about your project!
                 </Typography>
                 </Paper>
                 </Grid>
